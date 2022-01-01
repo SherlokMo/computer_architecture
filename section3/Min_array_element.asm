e64fdf1dce4857429a2ae8c1b9768f0a249de0a8
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2022-01-01 03:33:40 +0200

    Min array

---
+++ section3/Min_array_element.asm
@@ -0,0 +1,30 @@
+MOV [500], 5
+MOV [501], 40
+MOV [502], 30
+MOV [503], 50
+MOV [504], 20
+MOV [505], 10
+
+MOV SI, 500
+
+MOV Cl, [SI]
+
+INC SI
+MOV Al, [SI]  
+
+L1:
+    DEC Cl
+    CMP Cl, 00h
+    JE EXIT
+        INC SI
+        CMP Al, [SI]
+        JG L2
+        L2:
+            MOV Al, [SI] 
+        LOOP L1
+EXIT:
+    MOV [506], Al
+    HLT
+
+
+         
